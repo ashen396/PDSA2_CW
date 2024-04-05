7797fe5f9fcbc39b3a13cb06806fb27d40a418d8
Ashen <ashenrenon396@gmail.com>
2024-04-05 20:12:32 +0530

    Added images for testing

--- PDSA_2_CW/Shortest_Path.Designer.cs
+++ PDSA_2_CW/Shortest_Path.Designer.cs
@@ -28,12 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pnl_background = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // pnl_background
+            // 
+            this.pnl_background.AutoSize = true;
+            this.pnl_background.BackColor = System.Drawing.Color.White;
+            this.pnl_background.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnl_background.Location = new System.Drawing.Point(0, 0);
+            this.pnl_background.Name = "pnl_background";
+            this.pnl_background.Size = new System.Drawing.Size(800, 450);
+            this.pnl_background.TabIndex = 0;
+            this.pnl_background.Paint += new System.Windows.Forms.PaintEventHandler(this.pnl_background_Paint);
+            // 
+            // Shortest_Path
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pnl_background);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Shortest_Path";
             this.Text = "Shortest_Path";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnl_background;
     }
 }
--- PDSA_2_CW/Shortest_Path.cs
+++ PDSA_2_CW/Shortest_Path.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using PDSA_2_CW.Assets;
 
 namespace PDSA_2_CW
 {
@@ -15,6 +16,65 @@ namespace PDSA_2_CW
         public Shortest_Path()
         {
             InitializeComponent();
+        }
+
+        private void pnl_background_Paint(object sender, PaintEventArgs e)
+        {
+            //e.Graphics.DrawImage(Images.water, 100, 0);
+            //e.Graphics.DrawImage(Images.water, 148, 24);
+            //e.Graphics.DrawImage(Images.water, 196, 48);
+            //e.Graphics.DrawImage(Images.water, 244, 72);
+            //int x = 100;
+            //int y = 0;
+            //for (int counter = 0; counter <= 10; counter++)
+            //{
+            //    e.Graphics.DrawImage(Images.water, x, y);
+            //    x += 48;
+            //    y += 24;
+            //}
+
+            //7th row
+            //e.Graphics.DrawImage(Images.water, 308, 106);
+
+            //6th row
+            e.Graphics.DrawImage(Images.water, 260, 130);
+            e.Graphics.DrawImage(Images.water, 308, 154);
+
+            //5th row
+            e.Graphics.DrawImage(Images.water, 212, 154);
+            e.Graphics.DrawImage(Images.water, 356, 178);
+
+            //4th row
+            e.Graphics.DrawImage(Images.water, 164, 178);
+            e.Graphics.DrawImage(Images.water, 404, 202);
+
+            //3rd row
+            e.Graphics.DrawImage(Images.water, 116, 202);
+            e.Graphics.DrawImage(Images.water, 452, 226);
+
+            //2nd row
+            e.Graphics.DrawImage(Images.water, 68, 226);
+            e.Graphics.DrawImage(Images.water, 500, 250);
+            e.Graphics.DrawImage(Images.water, 452, 274);
+            e.Graphics.DrawImage(Images.water, 404, 298);
+            e.Graphics.DrawImage(Images.water, 356, 322);
+            e.Graphics.DrawImage(Images.water, 308, 346);
+
+            //1st row
+            e.Graphics.DrawImage(Images.water, 20, 250);
+            e.Graphics.DrawImage(Images.water, 68, 274);
+            e.Graphics.DrawImage(Images.water, 116, 298);
+            e.Graphics.DrawImage(Images.water, 164, 322); 
+            e.Graphics.DrawImage(Images.water, 212, 346);
+            e.Graphics.DrawImage(Images.water, 260, 370);
+
+            ////2nd row
+            //e.Graphics.DrawImage(Images.water, 75, 218);
+            //e.Graphics.DrawImage(Images.water, 68, 274);
+            //e.Graphics.DrawImage(Images.water, 116, 298);
+            //e.Graphics.DrawImage(Images.water, 164, 322);
+            //e.Graphics.DrawImage(Images.water, 212, 346);
+            //e.Graphics.DrawImage(Images.water, 260, 370);
         }
     }
 }
