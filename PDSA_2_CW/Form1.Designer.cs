afbf46b83548ec044b742455a174c2d713666797
FrozenFire <100995960+ForzenFire@users.noreply.github.com>
2024-04-06 15:32:48 +0530

    Predict The Value

---
+++ PDSA_2_CW/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace PDSA_2_Course_Work
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txt_index3 = new System.Windows.Forms.TextBox();
+            this.txt_index1 = new System.Windows.Forms.TextBox();
+            this.txt_index4 = new System.Windows.Forms.TextBox();
+            this.txt_index2 = new System.Windows.Forms.TextBox();
+            this.txt_userPrediction = new System.Windows.Forms.TextBox();
+            this.btn_CheckValue = new System.Windows.Forms.Button();
+            this.txt_expoTine = new System.Windows.Forms.TextBox();
+            this.txt_jumpTime = new System.Windows.Forms.TextBox();
+            this.txt_time = new System.Windows.Forms.TextBox();
+            this.txt_number = new System.Windows.Forms.TextBox();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txt_name);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.txt_userPrediction);
+            this.panel1.Controls.Add(this.btn_CheckValue);
+            this.panel1.Controls.Add(this.txt_expoTine);
+            this.panel1.Controls.Add(this.txt_jumpTime);
+            this.panel1.Controls.Add(this.txt_time);
+            this.panel1.Controls.Add(this.txt_number);
+            this.panel1.Controls.Add(this.btn_start);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(2, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(990, 536);
+            this.panel1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.groupBox1.Controls.Add(this.txt_index3);
+            this.groupBox1.Controls.Add(this.txt_index1);
+            this.groupBox1.Controls.Add(this.txt_index4);
+            this.groupBox1.Controls.Add(this.txt_index2);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(465, 173);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(410, 160);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Your Choices ";
+            // 
+            // txt_index3
+            // 
+            this.txt_index3.Location = new System.Drawing.Point(27, 90);
+            this.txt_index3.Name = "txt_index3";
+            this.txt_index3.Size = new System.Drawing.Size(169, 24);
+            this.txt_index3.TabIndex = 14;
+            // 
+            // txt_index1
+            // 
+            this.txt_index1.Location = new System.Drawing.Point(27, 48);
+            this.txt_index1.Name = "txt_index1";
+            this.txt_index1.Size = new System.Drawing.Size(169, 24);
+            this.txt_index1.TabIndex = 12;
+            // 
+            // txt_index4
+            // 
+            this.txt_index4.Location = new System.Drawing.Point(220, 90);
+            this.txt_index4.Name = "txt_index4";
+            this.txt_index4.Size = new System.Drawing.Size(169, 24);
+            this.txt_index4.TabIndex = 15;
+            // 
+            // txt_index2
+            // 
+            this.txt_index2.Location = new System.Drawing.Point(220, 48);
+            this.txt_index2.Name = "txt_index2";
+            this.txt_index2.Size = new System.Drawing.Size(169, 24);
+            this.txt_index2.TabIndex = 13;
+            // 
+            // txt_userPrediction
+            // 
+            this.txt_userPrediction.Location = new System.Drawing.Point(706, 354);
+            this.txt_userPrediction.Name = "txt_userPrediction";
+            this.txt_userPrediction.Size = new System.Drawing.Size(169, 22);
+            this.txt_userPrediction.TabIndex = 16;
+            this.txt_userPrediction.TextChanged += new System.EventHandler(this.txt_userPrediction_TextChanged);
+            // 
+            // btn_CheckValue
+            // 
+            this.btn_CheckValue.BackColor = System.Drawing.SystemColors.Desktop;
+            this.btn_CheckValue.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn_CheckValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_CheckValue.ForeColor = System.Drawing.Color.White;
+            this.btn_CheckValue.Location = new System.Drawing.Point(584, 407);
+            this.btn_CheckValue.Name = "btn_CheckValue";
+            this.btn_CheckValue.Size = new System.Drawing.Size(161, 52);
+            this.btn_CheckValue.TabIndex = 11;
+            this.btn_CheckValue.Text = "Check Index";
+            this.btn_CheckValue.UseVisualStyleBackColor = false;
+            this.btn_CheckValue.Click += new System.EventHandler(this.btn_CheckValue_Click);
+            // 
+            // txt_expoTine
+            // 
+            this.txt_expoTine.Location = new System.Drawing.Point(101, 311);
+            this.txt_expoTine.Name = "txt_expoTine";
+            this.txt_expoTine.Size = new System.Drawing.Size(256, 22);
+            this.txt_expoTine.TabIndex = 6;
+            // 
+            // txt_jumpTime
+            // 
+            this.txt_jumpTime.Location = new System.Drawing.Point(101, 265);
+            this.txt_jumpTime.Name = "txt_jumpTime";
+            this.txt_jumpTime.Size = new System.Drawing.Size(224, 22);
+            this.txt_jumpTime.TabIndex = 5;
+            // 
+            // txt_time
+            // 
+            this.txt_time.Location = new System.Drawing.Point(101, 223);
+            this.txt_time.Name = "txt_time";
+            this.txt_time.Size = new System.Drawing.Size(224, 22);
+            this.txt_time.TabIndex = 4;
+            // 
+            // txt_number
+            // 
+            this.txt_number.Location = new System.Drawing.Point(256, 127);
+            this.txt_number.Name = "txt_number";
+            this.txt_number.Size = new System.Drawing.Size(133, 22);
+            this.txt_number.TabIndex = 2;
+            // 
+            // btn_start
+            // 
+            this.btn_start.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btn_start.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_start.ForeColor = System.Drawing.Color.White;
+            this.btn_start.Location = new System.Drawing.Point(101, 114);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(112, 44);
+            this.btn_start.TabIndex = 1;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = false;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(235, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(510, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome to Predict the Value Index Game!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(439, 355);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(235, 18);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Please Enter Your Prediction :\r\n";
+            // 
+            // txt_name
+            // 
+            this.txt_name.Location = new System.Drawing.Point(651, 127);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(224, 22);
+            this.txt_name.TabIndex = 19;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(487, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(142, 18);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Enter Your Name:\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(988, 524);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txt_number;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_time;
+        private System.Windows.Forms.TextBox txt_jumpTime;
+        private System.Windows.Forms.TextBox txt_expoTine;
+        private System.Windows.Forms.Button btn_CheckValue;
+        private System.Windows.Forms.TextBox txt_index1;
+        private System.Windows.Forms.TextBox txt_index4;
+        private System.Windows.Forms.TextBox txt_index3;
+        private System.Windows.Forms.TextBox txt_index2;
+        private System.Windows.Forms.TextBox txt_userPrediction;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_name;
+    }
+}
+
---
+++ PDSA_2_CW/Form1.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static PDSA_2_Course_Work.Form1;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace PDSA_2_Course_Work
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        int[] randomArray;
+        BinarySearchTree bst;
+        private string connectionString;
+
+        public object Interaction { get; private set; }
+
+        //connectionString = "Data Source=KAVINDU;Initial Catalog=PredictTheValue;Integrated Security=True;Encrypt=True;";
+
+        public Form1()
+        {
+            InitializeComponent();
+            bst = new BinarySearchTree();
+        }
+        //private bool TestConnection()
+        //{
+        //    try
+        //    {
+        //        using (SqlConnection con = new SqlConnection(connectionString))
+        //        {
+        //            con.Open();
+        //            return true;
+        //        }
+        //    }
+        //    catch (SqlException)
+        //    {
+        //        return false;
+        //    }
+        //}
+        TimeSpan BinarySearchTime, jumpSearchTime, exponentialSearchTime;
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            randomArray = GenerateRandomArray(5000);
+            Array.Sort(randomArray);
+
+            foreach (int num in randomArray)
+            {
+                bst.Insert(num);
+            }
+
+            //if (!TestConnection())
+            //{
+            //    MessageBox.Show("Failed to establish database connection. Please check your connection string.", "Connection Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //    return;
+            //}
+
+            int targetValue = random.Next(1, 1000001);
+
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            int BinarySearchIndex = bst.Search(targetValue);
+            stopwatch.Stop();
+             BinarySearchTime = stopwatch.Elapsed;
+            stopwatch.Reset();
+
+            stopwatch.Start();
+            int jumpSearchIndex = JumpSearch(randomArray, targetValue);
+            stopwatch.Stop();
+             jumpSearchTime = stopwatch.Elapsed;
+            stopwatch.Reset();
+
+            stopwatch.Start();
+            int exponentialSearchIndex = ExponentialSearch(randomArray, targetValue);
+            stopwatch.Stop();
+             exponentialSearchTime = stopwatch.Elapsed;
+            stopwatch.Reset();
+
+            txt_number.Text = targetValue.ToString();
+
+            int predictedIndex = random.Next(0, randomArray.Length);
+
+            List<int> choices = new List<int>();
+            choices.Add(predictedIndex); 
+            while (choices.Count < 4)
+            {
+                int choice = random.Next(0, randomArray.Length);
+                if (!choices.Contains(choice))
+                    choices.Add(choice); 
+            }
+            choices.Sort();
+
+            txt_index1.Text = choices[0].ToString();
+            txt_index2.Text = choices[1].ToString();
+            txt_index3.Text = choices[2].ToString();
+            txt_index4.Text = choices[3].ToString();
+
+            txt_index1.Tag = predictedIndex;
+            txt_index2.Tag = predictedIndex;
+            txt_index3.Tag = predictedIndex;
+            txt_index4.Tag = predictedIndex;
+
+            //txt_index.Text = $"Binary Search Index: {BinarySearchIndex}{Environment.NewLine}" +
+            //                 $"Jump Search Index:{jumpSearchIndex}{Environment.NewLine}" +
+            //                 $"Exponentail Search Index: {exponentialSearchIndex}{Environment.NewLine}";
+
+            txt_time.Text = $"Binary Time Taken: {BinarySearchTime.TotalMilliseconds} ms";
+
+            txt_jumpTime.Text = $"Jump Time Taken: {jumpSearchTime.TotalMilliseconds} ms";
+
+            txt_expoTine.Text = $"Exponential Time Taken: {exponentialSearchTime.TotalMilliseconds} ms";
+
+        }
+
+        private int[] GenerateRandomArray(int length)
+        {
+            int[] array = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = random.Next(1, 1000001);
+            }
+            return array;
+        }
+
+        public class BinarySearchTree
+        {
+            private Node root;
+            public BinarySearchTree()
+            {
+                root = null;
+            }
+
+            public void Insert(int key)
+            {
+                root = InsertRec(root, key);
+            }
+
+            private Node InsertRec(Node root, int key)
+            {
+                if (root == null)
+                {
+                    root = new Node(key);
+                    return root;
+                }
+
+                if (key < root.data)
+                {
+                    root.left = InsertRec(root.left, key);
+                }
+                else if (key > root.data)
+                {
+                    root.right = InsertRec(root.right, key);
+                }
+
+                return root;
+            }
+
+            public int Search(int key)
+            {
+                return SearchRec(root, key);
+            }
+
+            private int SearchRec(Node root, int key)
+            {
+                if (root == null)
+                {
+                    return -1;
+                }
+
+                if (root.data == key)
+                {
+                    return root.data;
+                }
+
+                if (key < root.data)
+                {
+                    return SearchRec(root.left, key);
+                }
+                else
+                {
+                    return SearchRec(root.right, key);
+                }
+            }
+            private class Node
+            {
+                public int data;
+                public Node left, right;
+
+                public Node(int item)
+                {
+                    data = item;
+                    left = right = null;
+                }
+            }
+        }
+        private int JumpSearch(int[] array, int target)
+        {
+            int n = array.Length;
+            int step = (int)Math.Floor(Math.Sqrt(n));
+
+            int prev = 0;
+            while (array[Math.Min(step, n) - 1] < target)
+            {
+                prev = step;
+                step += (int)Math.Floor(Math.Sqrt(n));
+                if (prev >= n)
+                    return -1;
+            }
+
+            while (array[prev] < target)
+            {
+                prev++;
+                if (prev == Math.Min(step, n))
+                    return -1;
+            }
+
+            if (array[prev] == target)
+                return prev;
+
+            return -1;
+        }
+        private int ExponentialSearch(int[] array, int target)
+        {
+            int n = array.Length;
+            if (array[0] == target)
+                return 0;
+            int i = 1;
+            while (i < n && array[i] <= target)
+                i *= 2;
+            return BinarySearch(array, target, i / 2, Math.Min(i, n - 1));
+        }
+        private int BinarySearch(int[] array, int target, int low, int high)
+        {
+            while (low <= high)
+            {
+                int mid = low + (high - low) / 2;
+                if (array[mid] == target)
+                    return mid;
+                else if (array[mid] < target)
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+            return -1;
+        }
+        private void btn_CheckValue_Click(object sender, EventArgs e)
+        {
+            int predictedIndex = (int)groupBox1.Controls.OfType<TextBox>().FirstOrDefault(txt => txt.Name.StartsWith("txt_index") && txt.Tag != null && txt.Tag.ToString() == txt.Text)?.Tag;
+
+            int userPrediction;
+            if (!int.TryParse(txt_userPrediction.Text, out userPrediction))
+            {
+                MessageBox.Show("Please enter a valid number for prediction.", "Null Input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (userPrediction == predictedIndex)
+            {
+                if (string.IsNullOrWhiteSpace(txt_name.Text))
+                {
+                    MessageBox.Show("Please enter your name.", "Name Required", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                string userName = txt_name.Text;
+                //RecordPredictionResult(userName, Convert.ToDouble(txt_time.Text.Split(':')[1].TrimEnd(" ms".ToCharArray())),
+                //                                 Convert.ToDouble(txt_jumpTime.Text.Split(':')[1].TrimEnd(" ms".ToCharArray())),
+                //                                 Convert.ToDouble(txt_expoTine.Text.Split(':')[1].TrimEnd(" ms".ToCharArray())));
+
+                RecordPredictionResult(userName, BinarySearchTime, jumpSearchTime, exponentialSearchTime);
+
+                MessageBox.Show("Congratulations! Your prediction is correct.", "Prediction Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            else
+            {
+                MessageBox.Show($"Sorry, your prediction is incorrect. The correct index is {predictedIndex}.", "Prediction Result", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void RecordPredictionResult(string userName, TimeSpan BinarySearchTime, TimeSpan jumpSearchTime, TimeSpan exponentialSearchTime)
+        {
+            string query = "INSERT INTO Results (UserName, BinarySearchTime, JumpSearchTime, ExponentialSearchTime) " +
+               "VALUES (@UserName, @BinarySearchTime, @JumpSearchTime, @ExponentialSearchTime)";
+
+            connectionString = "Data Source=KAVINDU;Initial Catalog=PredictTheValue;Integrated Security=True;Encrypt=False";
+            using (SqlCommand command = new SqlCommand(query, new SqlConnection(connectionString)))
+            {
+                command.Parameters.AddWithValue("@UserName", userName);
+                command.Parameters.AddWithValue("@BinarySearchTime", BinarySearchTime.TotalMilliseconds);
+                command.Parameters.AddWithValue("@JumpSearchTime", jumpSearchTime.TotalMilliseconds);
+                command.Parameters.AddWithValue("@ExponentialSearchTime", exponentialSearchTime.TotalMilliseconds);
+
+                try
+                {
+                    command.Connection.Open();
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error inserting prediction result into database: " + ex.Message, "Database Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+        }
+        private void txt_userPrediction_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+    //public class ConnectionClass
+    //{
+    //    private string connectionString;
+
+    //    public ConnectionClass(string connectionString)
+    //    {
+    //        this.connectionString = connectionString;
+    //    }
+
+    //    public bool TestConnection()
+    //    {
+    //        try
+    //        {
+    //            using (SqlConnection con = new SqlConnection(connectionString))
+    //            {
+    //                con.Open();
+    //                return true;
+    //            }
+    //        }
+    //        catch (SqlException)
+    //        {
+    //            return false;
+    //        }
+    //    }
+    //}
